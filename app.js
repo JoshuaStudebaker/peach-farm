402f1df7ac9745d136328a0325106a36f1e1f3e8
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-08-21 15:11:23 -0600

    upload1

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ let trees = 1000;
 let dog = "";
 let farmerName = "";
 let orchardName = "";
+let cash = 10000;
+let daysLeft_Winter =  60;
+let daysLeft_Spring = 90;
+let daysLeft_Summer = 100;
+let winter = "Winter";
+let spring = "Spring"
+let summer = "Summer (aka the Harvest)";
 
 // items/upgrades
 let pruningItems = {
@@ -37,10 +44,53 @@ let pruningItems = {
 let thinningItems;
 let pickingItems;
 
-let workers;
+let farmWorkers = {
+  kid: {
+    name: "Kid",
+    pricePerDay: 20,
+    multiplier: 10,
+    experience: 2,
+  },
+  highSchooler: {
+    name: "High Schooler",
+    pricePerDay: 60,
+    multiplier: 8,
+    experience: 0,
+  },
+  goGetter: {
+    name: "Go Getter",
+    pricePerDay: 100,
+    multiplier: 20,
+    experience: 4,
+  },
+  farmLegend: {
+    name: "Farm Legend",
+    pricePerDay: 150,
+    multiplier: 35,
+    experience: 10,
+  },
+};
 
 // Element IDs
 let bushelsDisplay = document.getElementById("bushelCount");
+let timeOfYear = document.getElementById('time-of-year')
+// Intervals
+function dayCount{
+  daysLeft_Winter--
+  daysLeft_Spring--
+  daysLeft_Summer--
+}
+
+function season() {
+
+}
+function dayInterval() {
+  let days = setInterval(dayCount, 2000)
+}
+
+function drawSummer() {
+  let template
+}
 
 // Game
 function pick() {
